949b271950877c82e9c2ca6b43169bc94658edde
Divi Barvaliya <c0957873@mylambton.ca>
2025-11-17 20:06:26 -0500

    set up app js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// view engine setup
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "..", "views"));
+
+// static files
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// for handling form submissions
+app.use(express.urlencoded({ extended: true }));
+
+// use routes
+const pagesRouter = require("./src/routes/pages");
+app.use("/", pagesRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server is running at http://localhost:" + PORT);
+});
