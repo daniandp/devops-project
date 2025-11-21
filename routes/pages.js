7e94c7e63c71b6bae85e71aaf25b8c2fe4ce79ea
Divi Barvaliya <c0957873@mylambton.ca>
2025-11-20 21:00:14 -0500

    API enhancement for View support

--- routes/pages.js
+++ routes/pages.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { loginWithEmailPassword } = require("../services/authservice");
+const {
+  loginWithEmailPassword,
+  registerUser,
+  getUserProfileByEmail,
+} = require("../services/authservice");
 const router = express.Router();
 
 // redirect to login by default
@@ -45,27 +49,96 @@ router.get("/register", (req, res) => {
   res.render("register", { title: "Application Store - Register page" });
 });
 
+// POST /register - for form submit
+router.post("/register", async (req, res) => {
+  try {
+    const { email, password, firstName, middleName, lastName } = req.body;
+
+    const user = await registerUser({
+      email,
+      password,
+      firstName,
+      middleName,
+      lastName,
+    });
+
+    // if successful, redirect to the login page
+    return res.render("login", {
+      title: "Application Store - Login page",
+      message: "Registration successful. Please log in.",
+    });
+  } catch (error) {
+    console.log("error :: error on register submit ==>", error);
+
+    const status = error.statusCode || 500;
+
+    const errorMessage =
+      status === 400
+        ? error.message
+        : "Something went wrong. Please try again.";
+
+    return res.render("register", {
+      title: "Application Store - Register",
+      error: errorMessage,
+    });
+  }
+});
+
+// auth guard to prevent non logged in users to access dashboard and profile
+function requireLogin(req, res, next) {
+  if (!req.session || !req.session.user) {
+    return res.redirect("/login");
+  }
+  next();
+}
+
 // dashboard route
-router.get("/dashboard", (req, res) => {
-  const appliances = ["Fridge", "Washer", "Dryer"];
+router.get("/dashboard", requireLogin, (req, res) => {
+  // get user
+  const user = req.session.user;
+
+  // appliacances list
+  const appliances = [
+    { name: "Smart Fridge", price: 1200 },
+    { name: "Washing Machine", price: 800 },
+    { name: "Microwave Oven", price: 200 },
+  ];
+
+  // render page
   res.render("dashboard", {
     title: "Application Store - User Dashboard",
+    user,
     appliances,
   });
+});
+
+// profile route
+router.get("/profile", requireLogin, async (req, res) => {
+  try {
+    const email = req.session.user.email;
+
+    // get user profile by email
+    const userProfile = await getUserProfileByEmail(email);
+
+    // return response
+    res.render("profile", {
+      title: "Application Store - User Profile",
+      user: userProfile,
+    });
+  } catch (error) {
+    console.log("error :: error gettign user profile data ==>", error);
+    res.render("profile", {
+      title: "Application Store - Profile",
+      error: "Error loading User Profile. Please try again.",
+      user: null,
+    });
+  }
 });
 
 // logout route
 router.get("/logout", (req, res) => {
   req.session.destroy(() => {
     res.redirect("/login");
-  });
-});
-
-// profile route
-router.get("/profile", (req, res) => {
-  res.render("profile", {
-    title: "Application Store - User profile",
-    user,
   });
 });
 
